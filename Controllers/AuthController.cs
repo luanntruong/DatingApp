98263a25c52f320d529af6ca99012f86389d13cb
Luan Truong <luantruong210@gmail.com>
2018-03-14 21:56:28 -0700

    Added third party

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -53,8 +53,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Computer says no!");
-
             var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
 
             if (userFromRepo == null)
