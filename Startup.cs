0371e244f323b66fbbde63d05934edbe27b07239
Luan Truong <luantruong210@gmail.com>
2018-03-17 11:24:26 -0700

    Added photo upload function

--- Startup.cs
+++ Startup.cs
@@ -53,7 +53,9 @@ namespace DatingApp.API
                         ValidateIssuer = false,
                         ValidateAudience = false
                     };
-                });
+                }
+            );
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
